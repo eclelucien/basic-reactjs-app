2990a74227fed8c40532762e35a96488fc4384a5
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2023-05-15 23:53:33 -0300

    to lazzy to put a comment

--- src/App.js
+++ src/App.js
@@ -1,35 +1,80 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
 
 class App extends Component {
   state = {
-    clients: []
+    clients: [],
+    isLoading: true
   };
 
   async componentDidMount() {
     const response = await fetch('/clients');
     const body = await response.json();
-    this.setState({clients: body});
+    this.setState({ clients: body, isLoading: false });
+  }
+
+  async remove(id) {
+    await fetch(`/clients/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
+
+    let updatedClients = this.state.clients.filter(client => client.id !== id);
+    this.setState({ clients: updatedClients });
   }
 
   render() {
-    const {clients} = this.state;
+    const { clients, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    const clientList = clients.map(client => (
+      <tr key={client.id}>
+        <td style={{ whiteSpace: 'nowrap' }}>{client.name}</td>
+        <td>{client.email}</td>
+        <td>
+          <ButtonGroup>
+            <Button size="sm" color="primary" tag={Link} to={`/clients/${client.id}`}>
+              Edit
+            </Button>
+            <Button size="sm" color="danger" onClick={() => this.remove(client.id)}>
+              Delete
+            </Button>
+          </ButtonGroup>
+        </td>
+      </tr>
+    ));
+
     return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <div className="App-intro">
-              <h2>Clients</h2>
-              {clients.map(client =>
-                  <div key={client.id}>
-                    {client.name} ({client.email})
-                  </div>
-              )}
-            </div>
-          </header>
-        </div>
+      <div>
+        <AppNavbar />
+        <Container fluid>
+          <div className="float-right">
+            <Button color="success" tag={Link} to="/clients/new">
+              Add Client
+            </Button>
+          </div>
+          <h3>Clients</h3>
+          <Table className="mt-4">
+            <thead>
+              <tr>
+                <th width="30%">Name</th>
+                <th width="30%">Email</th>
+                <th width="40%">Actions</th>
+              </tr>
+            </thead>
+            <tbody>{clientList}</tbody>
+          </Table>
+        </Container>
+      </div>
     );
   }
 }
+
 export default App;
